6fbfd1d963dca550e1156b88b59aeb926a51d4fd
nguyentien208 <nguyentien2082102@gmail.com>
2024-06-01 11:47:32 +0700

    Fix line endings in main.css and output.js

--- public/output.js
+++ public/output.js
@@ -15,13 +15,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 });
 var swiper = new Swiper(".slideSwiper", {
-    slidesPerView: 3,
+    slidesPerView: 3.5,
     spaceBetween: 30,
 
     navigation: {
         nextEl: ".NextBut",
         prevEl: ".preBut",
     },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3.5,
+            spaceBetween: 30,
+        }
+    }
 });
 
 var swiper = new Swiper(".thietBiSwiper", {
@@ -31,4 +46,29 @@ var swiper = new Swiper(".thietBiSwiper", {
         nextEl: ".NextButtb",
         prevEl: ".preButtb",
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3.5,
+            spaceBetween: 30,
+        }
+    }
+});
+var thumbnail = document.getElementById("thumbnail");
+
+var video = document.getElementById("youtube-video");
+
+
+thumbnail.addEventListener("click", function () {
+
+    thumbnail.classList.add("hidden");
+
+    video.classList.remove("hidden");
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
 
